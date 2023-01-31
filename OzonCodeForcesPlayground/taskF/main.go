18db1e07bcbcef0bac62bb3bea0dfd7cf65fc7c1
geges1101 <62469977+geges1101@users.noreply.github.com>
2023-01-31 20:51:07 +0300

    Add files via upload

--- OzonCodeForcesPlayground/taskF/main.go
+++ OzonCodeForcesPlayground/taskF/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -24,11 +25,16 @@ func validate(begin []int, end []int) bool {
 		first = begin[0] < end[0]
 	}
 
-	second = begin[0] <= 24 && end[0] <= 24 &&
+	second = begin[0] <= 23 && end[0] <= 23 &&
 		begin[1] <= 59 && end[1] <= 59 &&
 		begin[2] <= 59 && end[2] <= 59
 
 	return first && second
+}
+
+type Pair struct {
+	begin []int
+	end   []int
 }
 
 func main() {
@@ -42,26 +48,44 @@ func main() {
 	for s := 0; s < sets; s++ {
 		var n int
 		fmt.Fscan(in, &n)
-		begin := make([]int, 3)
-		end := make([]int, 3)
+		all := make([]Pair, n)
 
 		flag := 1
 		for i := 0; i != n; i++ {
+			begin := make([]int, 3)
+			end := make([]int, 3)
 			var line string
 			fmt.Fscan(in, &line)
 			begin[0], _ = strconv.Atoi(line[0:2])
 			begin[1], _ = strconv.Atoi(line[3:5])
 			begin[2], _ = strconv.Atoi(line[6:8])
 			end[0], _ = strconv.Atoi(line[9:11])
-			begin[1], _ = strconv.Atoi(line[12:14])
-			begin[2], _ = strconv.Atoi(line[16:18])
-			if !validate(begin, end) {
-				break
+			end[1], _ = strconv.Atoi(line[12:14])
+			end[2], _ = strconv.Atoi(line[15:17])
+			all[i] = Pair{begin: begin, end: end}
+		}
+
+		sort.Slice(all, func(i, j int) bool {
+			return all[i].begin[0]*60*60+all[i].begin[1]*60+all[i].begin[2] <
+				all[j].begin[0]*60*60+all[j].begin[1]*60+all[j].begin[2]
+		})
+
+		for _, v := range all {
+			if !validate(v.begin, v.end) {
 				flag = -1
 				fmt.Println("NO")
+				break
 			}
 		}
 
+		for i := 0; i < n-1; i++ {
+			if all[i].end[0]*60*60+all[i].end[1]*60+all[i].end[2] >=
+				all[i+1].begin[0]*60*60+all[i+1].begin[1]*60+all[i+1].begin[2] {
+				flag = -1
+				fmt.Println("NO")
+				break
+			}
+		}
 		if flag != -1 {
 			fmt.Println("YES")
 		}
