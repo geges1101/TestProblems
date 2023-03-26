cff6ec5cb7733a6bad723cd2a97a7f1af44823b1
geges1101 <62469977+geges1101@users.noreply.github.com>
2023-03-26 22:52:39 +0300

    Add files via upload

--- задачи с собеседований яшки/longestSequenceOfOnes/main.go
+++ задачи с собеседований яшки/longestSequenceOfOnes/main.go
@@ -1,64 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 //Дан массив из нулей и единиц. Нужно определить, какой максимальный по длине подинтервал единиц можно получить, удалив ровно один элемент массива.
 //
 //[1, 1, 0]
 
-func maxOnes(nums []int) int {
-	res, start, end, n := 0, 0, 0, len(nums)
-
-	for i := 0; i < n; i++ {
-		if nums[i] == 1 {
-			start = i
-			end = start
-			for end < n {
-				if nums[end] == 1 {
-					end++
-				} else {
-					break
-				}
-			}
-			i = end
-			res = max(res, end-start)
+func longestSubarray(nums []int) int {
+	var start, curr, max int
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 0 {
+			curr++
 		}
-	}
-
-	for i := 1; i < n-1; i++ {
-		if nums[i-1] == 1 && nums[i] == 0 && nums[i+1] == 1 {
-			l, r := i-1, i+1
-			countl, countr := 0, 0
-			for l >= 0 {
-				if nums[l] == 1 {
-					countl++
-					l--
-				} else {
-					break
-				}
+		for curr > 1 {
+			if nums[start] == 0 {
+				curr--
 			}
-			for r < n {
-				if nums[r] == 1 {
-					countr++
-					r++
-				} else {
-					break
-				}
-			}
-			res = max(res, countl+1+countr)
+			start++
 		}
+		max = int(math.Max(float64(max), float64(i-start)))
 	}
-	return res
-}
-
-func max(a, b int) int {
-	if a >= b {
-		return a
-	} else {
-		return b
-	}
+	return max
 }
 
 func main() {
-	fmt.Print(maxOnes([]int{1, 0, 1, 0, 1, 1, 1, 0, 1}))
+	fmt.Print(longestSubarray([]int{1, 0, 1, 0, 1, 1, 1, 0, 1}))
 }
