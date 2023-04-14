391ca1a10b521b49576ddb3c41894f4e5056c24e
geges1101 <62469977+geges1101@users.noreply.github.com>
2023-04-14 20:21:43 +0300

    Add files via upload

---
+++ задачи с собеседований яшки/implement Rand10() Using Rand7()/go.mod
@@ -0,0 +1,3 @@
+module implement_Rand10()_Using_Rand7()
+
+go 1.19
---
+++ задачи с собеседований яшки/implement Rand10() Using Rand7()/main.go
@@ -0,0 +1,7 @@
+package main
+
+func rand10() int {
+	rand40:=40
+	for rand40>=40{rand40=(rand7()-1)*7+rand7()-1}
+	return rand40%10+1
+}
