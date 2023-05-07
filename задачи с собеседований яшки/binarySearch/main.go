c9d8e2c9f915b5a10cae540f172b9412f50618e4
geges1101 <62469977+geges1101@users.noreply.github.com>
2023-05-07 19:31:41 +0300

    Add files via upload

---
+++ задачи с собеседований яшки/binarySearch/main.go
@@ -0,0 +1,16 @@
+package binarySearch
+
+func search(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := l + (r-l)/2
+		if nums[mid] > target {
+			r = mid - 1
+		} else if nums[mid] < target {
+			l = mid + 1
+		} else {
+			return mid
+		}
+	}
+	return -1
+}
